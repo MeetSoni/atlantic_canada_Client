7d00d3552c317bb886b74c923f77e0411fc80895
Meet Soni <64322534+MeetSoni@users.noreply.github.com>
2024-03-12 03:52:27 -0400

    added_signup and login button to sidebar

--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -1,10 +1,23 @@
 // Sidebar.js
 import React from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+
+
 interface SidebarProps {
   closeMenu: () => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ closeMenu }) => {
+  const navigate = useRouter();
+  
+  const handleSignup=()=>{
+    navigate.push('/signUp');
+  }
+
+  const handleLogin=()=>{
+    navigate.push('/login');
+  }
   return (
     <div className="fixed z-10	 inset-0 bg-gray-800 bg-opacity-75 z-50">
       <div className="flex justify-end p-4">
@@ -24,6 +37,16 @@ const Sidebar: React.FC<SidebarProps> = ({ closeMenu }) => {
         </a>
         <a href="/Contact" className="text-white my-2">
           Contact
+        </a>
+      <a href="/login">
+      <button onClick={handleLogin}  className="text-white mt-2 bg-customRed py-2 px-4 rounded hover:bg-white hover:text-customRed hover:border hover:border-solid hover:border-customRed transition duration-300">
+            Login
+          </button>
+      </a>
+        <a href="/signUp">
+        <button onClick={handleSignup} className="text-white mt-2 bg-customRed py-2 px-4 rounded hover:bg-white hover:text-customRed hover:border hover:border-solid hover:border-customRed transition duration-300">
+            Signup
+          </button>
         </a>
       </div>
     </div>
