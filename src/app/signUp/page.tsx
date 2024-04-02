c7d30efb76d00389ef9030554ce459a8dd24c25c
Meet Soni <64322534+MeetSoni@users.noreply.github.com>
2024-04-01 21:34:46 -0400

    login,signup,context
    changed this pages

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import  API_URL  from '@/constants/constant';
+import { useAppContext } from '@/context';
 
 
 //defining type
@@ -16,6 +17,8 @@ interface FormData {
 
 //signup page function
 function SignupPage() {
+  const {authToken,setauthToken}=useAppContext();
+
     //state to handle form data
     const [formData, setFormData] = useState<FormData>({
         user_name: '',
@@ -115,11 +118,20 @@ function SignupPage() {
                         }
                     });
                     const data = await response.json();
-                    console.log(data.token);
-                    localStorage.setItem('token', data.token);
-                   
-                    console.log('Form Data:', formData);
+                    // console.log(data.message);
+
+                    if(data.message){
+                      setEmailError(`${data.message}`);
+                    }
+
+                    else{
+                      console.log(data.token)
+                      setauthToken(data.token); // Setting the authToken from the response
+                      console.log(`Auth Token: ${authToken}`);
+                        alert("signup successfully")
                     navigate.push('/');
+                    }
+                 
                 }
                 
                 catch (err: any) {
@@ -135,58 +147,44 @@ function SignupPage() {
 // returning the body 
   return (
     <>
-
-      <div className=" p-10 min-h-screen flex items-center justify-center bg-gray-100  bg-gradient-to-r from-grad_red to-grad_white">
-        <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-          <h2 className="text-3xl font-bold mb-6">Signup</h2>
-         {error && <p style={{ color: 'red' }}>{error}</p>}
-
-          <form id="signup-form" className="space-y-4" onSubmit={handleSubmit}>
-         {commonError && <p style={{ color: 'red' }}>{commonError}</p>}
-
-            <div>
-              <label htmlFor="user_name" className="block text-sm font-medium text-gray-600">  Username <span style={{ color: 'red' }}>*</span></label> 
-              <input onChange={(e)=>handleChange(e)} type="text" id="user_name" name="user_name" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
-            </div>
-          {usernameError && <p style={{ color: 'red' }}>{usernameError}</p>}
-
-            
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email <span style={{ color: 'red' }}>*</span></label>
-              <input onChange={(e)=>handleChange(e)} type="text" id="email" name="email" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
-            </div>
-          {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
-
-            
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password <span style={{ color: 'red' }}>*</span></label>
-              <input onChange={(e)=>handleChange(e)} type="password" id="password" name="password" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
-            </div>
-          {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
-
-
-        
-
-            <div>
-              <label htmlFor="user_type" className="block text-sm font-medium text-gray-600">User Type <span style={{ color: 'red' }}>*</span></label>
-              <input onChange={(e)=>handleChange(e)} type="text" id="user_type" name="user_type" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
-            </div>
-          {usertypeError && <p style={{ color: 'red' }}>{usertypeError}</p>}
-
-            <div>
-              <label htmlFor="province" className="block text-sm font-medium text-gray-600">Province <span style={{ color: 'red' }}>*</span></label>
-              <input onChange={(e)=>handleChange(e)} type="text" id="province" name="province" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
-            </div>
-          {provinceError && <p style={{ color: 'red' }}>{provinceError}</p>}
-            
-            
-            
-            <button type="submit" className="w-full bg-customRed text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500">
-              Signup
-            </button>
-          </form>
-        </div>
+<div className="p-10 items-center justify-center bg-gray-100 bg-gradient-to-r from-grad_red to-grad_white">
+  <div className="bg-white p-8  rounded shadow-md  max-w-md mx-20">
+    <h2 className="text-3xl font-bold mb-6">Signup</h2>
+    {error && <p className="text-red-500">{error}</p>}
+    <form id="signup-form" className="space-y-4" onSubmit={handleSubmit}>
+      {commonError && <p className="text-red-500">{commonError}</p>}
+      <div>
+        <label htmlFor="user_name" className="block text-sm font-medium text-gray-600">Username <span className="text-red-500">*</span></label>
+        <input onChange={(e)=>handleChange(e)} type="text" id="user_name" name="user_name" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
       </div>
+      {usernameError && <p className="text-red-500">{usernameError}</p>}
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email <span className="text-red-500">*</span></label>
+        <input onChange={(e)=>handleChange(e)} type="text" id="email" name="email" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      {emailError && <p className="text-red-500">{emailError}</p>}
+      <div>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password <span className="text-red-500">*</span></label>
+        <input onChange={(e)=>handleChange(e)} type="password" id="password" name="password" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      {passwordError && <p className="text-red-500">{passwordError}</p>}
+      <div>
+        <label htmlFor="user_type" className="block text-sm font-medium text-gray-600">User Type <span className="text-red-500">*</span></label>
+        <input onChange={(e)=>handleChange(e)} type="text" id="user_type" name="user_type" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      {usertypeError && <p className="text-red-500">{usertypeError}</p>}
+      <div>
+        <label htmlFor="province" className="block text-sm font-medium text-gray-600">Province <span className="text-red-500">*</span></label>
+        <input onChange={(e)=>handleChange(e)} type="text" id="province" name="province" className="mt-1 p-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      {provinceError && <p className="text-red-500">{provinceError}</p>}
+      <button type="submit" className="w-full bg-customRed text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500">
+        Signup
+      </button>
+    </form>
+  </div>
+</div>
+
     </>
 
   );
