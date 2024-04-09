bb172ec329c6c1a0add2a2143020e4a02e904746
Meet Soni <64322534+MeetSoni@users.noreply.github.com>
2024-04-09 02:37:44 -0400

    added signup new

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -7,12 +7,16 @@ export default {
   LOGOUT: `${API_URL}auth/logout`,
   SIGNUP: `${API_URL}user/signup`,
   FORGOT_PASSWORD: `${API_URL}user/send-reset-password-email`,
-  GET_SERVICE_DATA: `${API_URL}services`,
+  GET_SERVICE_DATA: `${API_URL}services/subservices`,
+  GET_ALL_SERVICE_DATA: `${API_URL}services`,
   YOUTUBE:`${API_URL}youtube`,
   RESETPASSWORD:`${API_URL}user/reset-password`,
+  PROVINCE_BY_ID:`${API_URL}province`,
   ABOUT_US:`${API_URL}about_us`,
+  GET_USERDATA_BY_ID:`${API_URL}getdatabyemail`,
+  GET_ALL_HOME_PROVINCES:`${API_URL}home_provinces`,
+  ADD_PROFILE_PHOTO_URL:`${API_URL}addprofilepic`,
+  UPDATE_PROFILE_DATA:`${API_URL}profileupdate`,
   SEND_CONTACT_INFO:`${API_URL}user/sendcontactinfo`
-
-  
 
 };
